d5668e3a2d78328478fe7670556cde0ca37161c9
Felipe Cabrera <me@felieppe.com>
2025-05-24 03:06:05 -0300

    feat: add access token generator for auth/login

---
+++ app/utils/auth.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from typing import Any
+from jose import jwt
+
+from app.config import settings
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Create a JWT access token with an expiration time.
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+    return encoded_jwt
