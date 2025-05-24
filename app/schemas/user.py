2875f9a90dba20b34bf928e3d7d9bf4dba1e9e4b
Felipe Cabrera <me@felieppe.com>
2025-05-24 14:01:45 -0300

    feat: add user schema

---
+++ app/schemas/user.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserBase(BaseModel):
+    correo: EmailStr = Field(..., example="usuario@empresa.com")
+    es_administrador: bool = Field(False, description="Indica si el usuario es administrador.")
+
+class UserCreate(UserBase):
+    contraseña: str = Field(..., example="password", min_length=8, description="Contraseña del usuario, debe tener al menos 8 caracteres.")
